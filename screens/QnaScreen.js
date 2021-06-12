c5a7c2836ae993711b51ede6222992af764c7477
Kurt VanDusen <info@equationalapplications.com>
2021-06-12 18:13:45 -0400

    Change passage

--- screens/QnaScreen.js
+++ screens/QnaScreen.js
@@ -16,10 +16,10 @@ export default class QnaScreen extends React.Component {
     this.state = {
       isTfReady: false,
       isModelReady: false,
-      default_question: "What is a google?",
+      default_question: "Where is a google?",
       default_passage: "Google absorbs technology that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware. It is considered one of the Big Four technology companies, alongside Amazon, Apple, and Facebook. Google was founded in September 1998 by Larry Page and Sergey Brin while they were Ph.D. students at Stanford University in California. Together they own about 14 percent of its shares and control 56 percent of the stockholder voting power through supervoting stock. They incorporated Google as a California privately held company on September 4, 1998, in California. Google was then reincorporated in Delaware on October 22, 2002. An initial public offering (IPO) took place on August 19, 2004, and Google moved to its headquarters in Mountain View, California, nicknamed the Googleplex. In August 2015, Google announced plans to reorganize its various interests as a conglomerate called Alphabet Inc. Google is Alphabet's leading subsidiary and will continue to be the umbrella company for Alphabet's Internet interests. Sundar Pichai was appointed CEO of Google, replacing Larry Page who became the CEO of Alphabet.",
       question: ' default question',
-      passage: ' default pessage',
+      passage: ' default passage',
       answers: null,
     };
   }
