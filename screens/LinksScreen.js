e6b601a1fa667161a8ad4d226bdde5cecc647fc1
Kurt VanDusen <info@equationalapplications.com>
2021-06-14 00:40:36 -0400

    Copy files for qna demo

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,13 +1,59 @@
 //import { Ionicons } from '@expo/vector-icons';
 //import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
-export default function LinksScreen() {
-  return (
-    <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-      {/*<OptionButton
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
+
+import * as qna from '../components/qna/qna'
+import renderIf from '../components/helpers/renderIf'
+
+const passage = 'The wonderful world of very good programming pervades human existance.';
+const question = 'What is it about?';
+
+export default class LinksScreen extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      isTfReady: false,
+    }
+  }
+  async componentDidMount() {
+    await tf.ready();
+    this.model = await qna.load().catch(e => TTS('Need to load the q n a' + e));
+    this.setState({ isTfReady: true });
+  }
+
+  askQusetion = async () => {
+
+    const answers = await this.model.findAnswers(question, passage).catch(e => console.log('My head hurts.' + e));
+
+    if (answers[0].text.length > answers[1].text.length && answers[0].text.length > answers[2].text.length) {
+      console.log(answers[0].text);
+      this.setState({ status: answers[0].text });
+    }
+    else if (answers[1].text.length > answers[2].text.length) {
+      console.log(answers[1].text);
+    }
+    else {
+      console.log(answers[2].text);
+    }
+  }
+
+  render() {
+    return (
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View>
+          {renderIf(this.state.isTfReady,
+            <Button
+              title='ask'
+              onPress={() => this.askQusetion()}
+            />
+          )}
+        </View>
+        {/*<OptionButton
         icon="md-school"
         label="Read the Expo documentation"
         onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
@@ -25,10 +71,10 @@ export default function LinksScreen() {
         onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
         isLastOption
       />*/}
-    </ScrollView>
-  );
+      </ScrollView>
+    );
+  }
 }
-
 /*function OptionButton({ icon, label, onPress, isLastOption }) {
   return (
     <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
