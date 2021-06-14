a04c89e719a6b8dbf3e3b96ef2a18f7c88a27e4a
Kurt VanDusen <info@equationalapplications.com>
2021-06-14 15:11:12 -0400

    Ask Alice code working with console.log

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,34 +1,56 @@
 //import { Ionicons } from '@expo/vector-icons';
 //import * as WebBrowser from 'expo-web-browser';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import { RectButton, ScrollView, TouchableNativeFeedback } from 'react-native-gesture-handler';
 
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-react-native';
 
+import Fuse from 'fuse.js';
+
 import * as qna from '../components/qna/qna'
 import renderIf from '../components/helpers/renderIf'
-
-const passage = 'The wonderful world of very good programming pervades human existance.';
-const question = 'What is it about?';
+import { passageJson } from '../components/aliceinwonderland';
 
 export default class LinksScreen extends Component {
+
   constructor(props) {
     super();
     this.state = {
+      question: 'What is your question?',
       isTfReady: false,
     }
   }
+
   async componentDidMount() {
+    const optionsFuse = {
+      isCaseSensitive: false,
+      ignoreLocation: true,
+      findAllMatches: true,
+      ignoreFieldNorm: true,
+      keys: ['passage']
+    };
+    // Create the Fuse index
+    // const passageIndex = Fuse.createIndex(optionsFuse.keys, passageJson);
+    // this.fuse = new Fuse(passageJson, optionsFuse, passageIndex);
+    this.fuse = new Fuse(passageJson, optionsFuse);
+
     await tf.ready();
-    this.model = await qna.load().catch(e => TTS('Need to load the q n a' + e));
+    this.model = await qna.load().catch(e => console.log('Need to load the q n a' + e));
     this.setState({ isTfReady: true });
   }
 
-  askQusetion = async () => {
+  onChangeText = (text) => {
+    this.setState({ question: text });
+  }
 
-    const answers = await this.model.findAnswers(question, passage).catch(e => console.log('My head hurts.' + e));
+  askQusetion = async (text) => {
+
+    const result = this.fuse.search(text);
+
+    const answers = await this.model.findAnswers(text, result[0].item.passage).catch(e => console.log('My head hurts.' + e));
+    //const answers = await this.model.findAnswers(text, passageJson.passage[0]).catch(e => console.log('My head hurts.' + e));
 
     if (answers[0].text.length > answers[1].text.length && answers[0].text.length > answers[2].text.length) {
       console.log(answers[0].text);
@@ -46,49 +68,28 @@ export default class LinksScreen extends Component {
     return (
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
         <View>
+          <TextInput
+            placeholder="Type here to ask!"
+            onChangeText={this.onChangeText}
+            value={this.state.question}
+          >
+
+          </TextInput>
           {renderIf(this.state.isTfReady,
-            <Button
-              title='ask'
-              onPress={() => this.askQusetion()}
-            />
+
+            <TouchableOpacity
+              onPress={() => this.askQusetion(this.state.question)}>
+              <Text>Ask</Text>
+            </TouchableOpacity>
+
           )}
+          <Text>{this.state.question}</Text>
         </View>
-        {/*<OptionButton
-        icon="md-school"
-        label="Read the Expo documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://docs.expo.io')}
-      />
-
-      <OptionButton
-        icon="md-compass"
-        label="Read the React Navigation documentation"
-        onPress={() => WebBrowser.openBrowserAsync('https://reactnavigation.org')}
-      />
-
-      <OptionButton
-        icon="ios-chatboxes"
-        label="Ask a question on the forums"
-        onPress={() => WebBrowser.openBrowserAsync('https://forums.expo.io')}
-        isLastOption
-      />*/}
       </ScrollView>
     );
   }
 }
-/*function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton style={[styles.option, isLastOption && styles.lastOption]} onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
-  );
-}*/
+
 
 const styles = StyleSheet.create({
   container: {
